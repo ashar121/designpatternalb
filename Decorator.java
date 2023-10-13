8060dabb9c3b6654df54872dd0ba64ee53696dad
ashar121 <108254340+ashar121@users.noreply.github.com>
2023-10-13 21:33:16 +0500

    Add files via upload

---
+++ Decorator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.decorator;
+
+/**
+ *
+ * @author Ashar's Book
+ */
+public class Decorator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
