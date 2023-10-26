7d9048a474f310a4b22d44dc6d01201e26364dc5
ashar121 <108254340+ashar121@users.noreply.github.com>
2023-10-26 09:24:54 +0500

    Add files via upload

---
+++ abstractfactory/DoubleBorderSquare.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstractfactory;
+
+/**
+ *
+ * @author fa20-bse-158
+ */
+
+
+public class DoubleBorderSquare implements DoubleBorderShape {
+
+  @Override
+  public void draw() {
+    System.out.println("Inside DoubleBorderSquare::draw() method.");
+  }
+
+  @Override
+  public void drawDoubleBorder() {
+    System.out.println("Drawing double border around square...");
+  }
+}
