86fe31162e71e0b22174dd441bb0541186d2a9ec
ashar121 <108254340+ashar121@users.noreply.github.com>
2023-11-25 22:35:10 +0500

    adapter lab

---
+++ adapter/src/main/java/com/mycompany/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.adapter;
+
+/**
+ *
+ * @author Ashar's Book
+ */
+public interface AdvancedMediaPlayer {	
+   public void playVlc(String fileName);
+   public void playMp4(String fileName);
+}
---
+++ adapter/src/main/java/com/mycompany/adapter/MediaPlayer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adapter;
+
+/**
+ *
+ * @author Ashar's Book
+ */
+public interface MediaPlayer {
+   public void play(String audioType, String fileName);
+}
