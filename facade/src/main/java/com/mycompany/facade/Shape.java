0f6019910aace395c457f4b79632fbb5fd769b21
ashar121 <108254340+ashar121@users.noreply.github.com>
2023-11-25 22:40:54 +0500

    facade

---
+++ facade/src/main/java/com/mycompany/facade/Shape.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facade;
+
+/**
+ *
+ * @author Ashar's Book
+ */
+public interface Shape {
+   void draw();
+}
