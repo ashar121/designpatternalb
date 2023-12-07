b8fc70ebccc88d0ffdc26b0f2c1b6408006dcdf3
ashar121 <108254340+ashar121@users.noreply.github.com>
2023-12-07 20:27:43 +0500

    composite lab

---
+++ LabTask_CompositePattern/DOO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package composite_tutorialspoint;
+
+
+public class DOO extends BaseEmployee {
+    private String rank;
+
+    public DOO(String name, int salary, String rank) {
+        super(name, salary);
+        this.rank = rank;
+    }
+
+    @Override
+    public void giveBonus(int percentage) {
+         int bonus = (( salary / 100 ) * percentage);
+         System.out.println("Hello, my name is " + name + " and I am DOO with rank" + rank + " and I got bonus: " + bonus);
+    }
+    
+}
